238096718d805a4a9fe76575e20800458b928006
shaguftha16 <shaguftha4324@gmail.com>
2026-07-06 14:44:03 +0530

    added array programns

---
+++ singlearray.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class singlearray {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements:");
+
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array elements are:");
+
+        
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+
+        
+    }
+}
+
